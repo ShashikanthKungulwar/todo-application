81e50177025e7d9c3f7f81a45129e15380d5982d
shashikanth <shashikanthk80@gmail.com>
2023-10-18 12:00:57 +0530

    complete basic setup involving folder creation and assets

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const PORT=8000;
+
+
+const app=express();
+app.use(express.static('assets'));
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use('/',require('./routes/index.js'));
+app.listen(PORT,(err)=>{
+    if(err)
+    {
+        console.error("something went wrong",err);
+        return;
+    }
+    console.log("server is running at port: ",PORT);
+});
